60b0fb2b75a146b9195c33f259b872c7e14efc06
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-15 14:50:03 -0500

    l

--- Program.cs
+++ Program.cs
@@ -4,8 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-           
+            // integers
+            Console.WriteLine(Calculate.Add(15, 88));
+            Console.WriteLine(Calculate.Sub(78, 88));
+            Console.WriteLine(Calculate.Mult(8, 9));
+            Console.WriteLine(Calculate.Div(77, 8));
+
+            //floats
+            Console.WriteLine(Calculate.Add(15.55f, 88.65f));
+            Console.WriteLine(Calculate.Sub(78.45f, 88.89f));
+            Console.WriteLine(Calculate.Mult(8.99f, 9.65f));
+            Console.WriteLine(Calculate.Div(77.265f, 8.0f));
+
         }
     }
 }
